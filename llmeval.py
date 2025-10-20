28e76fb52c2ecb1ef93e00e3e7a6bfc034554d38
Mathieu Virbel <mat@meltingrocks.com>
2025-10-20 16:22:17 -0600

    fix: opencode support

--- llmeval.py
+++ llmeval.py
@@ -39,7 +39,7 @@ CUBBIX_COMMAND_TEMPLATE = [
     "--run",
     """
     if [ -f install.sh ]; then bash install.sh; fi;
-    cd input && goose run -i ../task.md
+    cd input && opencode run --print-logs --format json < ../task.md
     """,
     ".",
 ]
